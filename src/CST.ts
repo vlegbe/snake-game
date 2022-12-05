19aae67fc0cafb8dffa88190bc533ae8a019f7eb
Victoria Egbe <vlegbe@hotmail.com>
2022-12-05 01:44:32 +0000

    updated game

---
+++ src/CST.ts
@@ -0,0 +1,19 @@
+export const CST = {
+    SCENES: {
+        LOAD: "LOAD",
+        MENU: "MENU",
+        PLAY: "PLAY"
+    },
+    IMAGE: {
+        LOGO: "../assets/snake.png",
+        OPTIONS: "../assets/options_button.png",
+        PLAY: "../assets/play_button.png",
+        TITLE: "../assets/snake_game_logo.jpeg"
+    },
+    AUDIO: {
+        TITLE: "../assets/dist_assets_audio_shuinvy-childhood.mp3"
+    },
+    SPRITE: {
+        SNAKE: "../assets/red.png"
+    }
+}
